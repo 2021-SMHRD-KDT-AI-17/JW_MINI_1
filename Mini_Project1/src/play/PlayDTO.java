e700b7ddb7b8bc626e6c5cb7bfe944398af5d857
smhrd <smhrd@59.0.236.149>
2024-01-26 17:42:55 +0900

    playDTO 수정

--- Mini_Project1/src/play/PlayDTO.java
+++ Mini_Project1/src/play/PlayDTO.java
@@ -2,4 +2,37 @@ package play;
 
 public class PlayDTO {
 
+	private int money;
+	private int hp;
+	private int cnt_date;
+	private String id;
+	
+	
+	public int getMoney() {
+		return money;
+	}
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	public int getHp() {
+		return hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	public int getCnt_date() {
+		return cnt_date;
+	}
+	public void setCnt_date(int cnt_date) {
+		this.cnt_date = cnt_date;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+	
 }
